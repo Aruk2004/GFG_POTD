75bf283d5d3a8d6ed2c3a0494f2a705b69924c4f
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2023-09-23 14:35:43 +0530

    Create Sep_23.py

---
+++ Sep_2023/Sep_23.py
@@ -0,0 +1,21 @@
+# Equilibriam Point
+
+class Solution:
+    def equilibriumPoint(self, a, n):
+        total_sum = sum(a)
+        pre_sum = 0
+        
+        for i in range(n):
+            total_sum -= a[i]
+            if total_sum == pre_sum:
+                return i + 1
+            pre_sum += a[i]
+        
+        return -1
+
+# Example usage:
+arr = [1, 3, 5, 2, 2]
+n = len(arr)
+sol = Solution()
+result = sol.equilibriumPoint(arr)
+print(result)  # Output: 3
