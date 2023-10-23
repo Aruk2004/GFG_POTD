f6f63628ce8a643c29c68bf85c7cf8d17375db52
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2023-10-23 16:32:54 +0530

    Create Oct_23.py

---
+++ Oct_2023/Oct_23.py
@@ -0,0 +1,23 @@
+# Maximum sum increasing subsequence
+
+class Solution:
+    def maxSumIS(self, arr, n):
+        dp = list(arr)
+
+        for i in range(1, n):
+            for j in range(i):
+                if arr[j] < arr[i]:
+                    dp[i] = max(dp[i], dp[j] + arr[i])
+
+        out = 0
+        for i in dp:
+            out = max(out, i)
+        return out
+
+# Example usage:
+sol = Solution()
+arr = [1, 101, 2, 3, 100, 4, 5]
+n = len(arr)
+
+result = sol.maxSumIS(arr, n)
+print("Maximum sum of increasing subsequence:", result)  # Output: Maximum sum of increasing subsequence: 106
