c51e6ae6096c1bc8f7878c49a7c6c4833ac09a53
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2023-10-27 14:49:09 +0530

    Minimum deletations to make string palindrome

---
+++ Oct_2023/Oct_27.py
@@ -0,0 +1,23 @@
+# Minimum deletations to make string palindrome
+
+class Solution:
+    def minimumNumberOfDeletions(self, s):
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+        lps_length = dp[0][n - 1]
+        return n - lps_length
+
+# Example usage:
+sol = Solution()
+S = "abcd"
+result = sol.minimumNumberOfDeletions(S)
+print("Minimum number of deletions to make", S, "a palindrome:", result)
