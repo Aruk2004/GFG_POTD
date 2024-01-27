f513674773da1a4a29536622c9a02dc4f5ff9ec0
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2024-01-27 19:02:35 +0530

    Brackets in Matrix Chain Multiplication
    Given an array p[] of length n used to denote the dimensions of a series of matrices such that the dimension of i'th matrix is p[i] * p[i+1]. There are a total of n-1 matrices. Find the most efficient way to multiply these matrices together. 
    As in MCM, you were returning the most effective count but this time return the string which is formed of A - Z (only Uppercase) denoting matrices & Brackets( "(" ")" ) denoting multiplication symbols. For example, if n =11, the matrixes can be denoted as A - K as n<=26 & brackets as multiplication symbols.
    
    NOTE:
    
    Each multiplication is denoted by putting open & closed brackets to the matrices multiplied & also Please note that the order of matrix multiplication matters, as matrix multiplication is non-commutative A*B != B*A
    As there can be multiple possible answers, the console would print "True" for the correct string and "False" for the incorrect string. You need to only return a string that performs a minimum number of multiplications.

---
+++ Jan_2024/Jan_27.py
@@ -0,0 +1,43 @@
+# Brackets in Matrix Chain Multiplication
+
+class Solution:
+    
+    def matrixChainOrder(self, p, n):
+        v = [[("", -1) for _ in range(n)] for _ in range(n)]
+
+        operations = self.solve(p, n, 1, n - 1, v)
+
+        return operations[0]
+
+    def solve(self, p, n, i, j, v):
+        if i == j:
+            s = chr(ord('A') + i - 1)
+            return (s, 0)
+
+        if v[i][j][1] != -1:
+            return v[i][j]
+
+        mini = float('inf')
+        sr = ""
+        for k in range(i, j):
+            left = self.solve(p, n, i, k, v)
+            right = self.solve(p, n, k + 1, j, v)
+
+            steps = left[1] + right[1] + p[i - 1] * p[k] * p[j]
+
+            if steps < mini:
+                sr = '(' + left[0] + right[0] + ')'
+                mini = steps
+
+        v[i][j] = (sr, mini)
+        return v[i][j]
+
+
+# Example usage:
+solution = Solution()
+p = [1, 2, 3, 4, 5]
+n = len(p)
+result = solution.matrixChainOrder(p, n)
+
+# Print the result
+print(result)
