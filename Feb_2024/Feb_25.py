5d5fb8391771308aba0e62ca2d163c57b4494483
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2024-02-25 15:14:33 +0530

    Reach a given score
    Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n, find number of distinct combinations to reach the given score.

---
+++ Feb_2024/Feb_25.py
@@ -0,0 +1,22 @@
+# Reach a given score
+
+class Solution:
+    def count(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        
+        for i in range(3, n + 1):
+            dp[i] += dp[i - 3]
+            
+        for i in range(5, n + 1):
+            dp[i] += dp[i - 5]
+            
+        for i in range(10, n + 1):
+            dp[i] += dp[i - 10]
+
+        return dp[n]
+
+# Example usage:
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.count(15))  # Output: 4
