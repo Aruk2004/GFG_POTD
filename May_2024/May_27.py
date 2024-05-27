b9865b58ce2872e0873bf196b8405a3a32e2926e
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2024-05-27 13:55:24 +0530

    Longest subsequence-1
    Given an integer array a[] of size n, find the length of the longest subsequence such that the absolute difference between adjacent elements is 1.

---
+++ May_2024/May_27.py
@@ -0,0 +1,14 @@
+# Longest subsequence-1
+
+from typing import List
+
+class Solution:
+    def longestSubseq(self, N, A):
+        dp, max1 = [1] * N, float("-inf")
+
+        for i in range(1, N):
+            for j in range(i):
+                if abs(A[i] - A[j]) == 1:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    max1 = max(max1, dp[i])
+        return max1
