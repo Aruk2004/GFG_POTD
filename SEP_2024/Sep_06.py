e9ac79fc32fe25840d1e5897d1747d1eba87ab12
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2024-09-06 20:04:48 +0530

    Kadane's Algorithm

---
+++ SEP_2024/Sep_06.py
@@ -0,0 +1,12 @@
+# Kadane's Algorithm
+
+class Solution:
+
+    def maxSubArraySum(self,arr):
+
+        max_so_far = arr[0]
+        current_max = arr[0]
+        for i in range(1, len(arr)):
+            current_max = max(arr[i], current_max + arr[i])
+            max_so_far = max(max_so_far, current_max)
+        return max_so_far
