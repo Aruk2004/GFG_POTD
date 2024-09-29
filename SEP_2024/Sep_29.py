5587f89ee9d10cda584f7b16db4284176380a838
Aruk Bandyopadhyay <114574020+Aruk2004@users.noreply.github.com>
2024-09-29 20:44:34 +0530

    Total count
    You are given an array arr[] of positive integers and a threshold value k. For each element in the array, divide it into the minimum number of small integers such that each divided integer is less than or equal to k. Compute the total number of these integer across all elements of the array.

---
+++ SEP_2024/Sep_29.py
@@ -0,0 +1,11 @@
+# Total count
+
+class Solution:
+    def totalCount(self, k, arr):
+        c = 0
+        for i in arr:
+            if i % k == 0:
+                c = c + (i // k)
+            else:
+                c = c + (i // k) + 1
+        return c
